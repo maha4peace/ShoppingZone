ad9dcf41843ae20af24e99726855d57631a8fe85
Maha <maha.ahmed4peace@gmail.com>
2024-01-30 09:38:29 -0500

    adding more properties to the products object

--- src/App.js
+++ src/App.js
@@ -4,42 +4,69 @@ import { useState } from 'react';
 import Product from './components/Products';
 import Cartitem from './components/Cartitem';
 
+const categories = ["tool", "food", "clothing"]
+
 const products = [
   {
     name: "allen wrench",
     price: 2.99,
-    description: "handy tool"
+    description: "handy tool",
+    category: "tool"
   }, 
   {
     name: "cornucopia",
     price: 5.99,
-    description: "destive holiday decoration"
+    description: "destive holiday decoration",
+    category: "tool"
   },
   {
     name: "banana",
     price: 0.99,
-    description: "fruit of potassium"
+    description: "fruit of potassium",
+    category: "food"
   },
   {
     name: "jack-o-lantern",
     price: 3.99,
-    description: "spooky seasonal fun"
+    description: "spooky seasonal fun",
+    category: "food"
   },
   {
     name:"doggie treat box",
     price: 5.99,
-    description: "fido loves 'em"
+    description: "fido loves 'em",
+    category: "food"
   },
   {
     name: "LED lightbulb",
     price: 6.55,
-    description: "It's super efficient!"
+    description: "It's super efficient!",
+    category: "tool"
   },
   {
     name: "turtleneck",
     price: 19.99,
-    description: "available in black!"
+    description: "available in black!",
+    category: "clothing"
+  }, 
+  { name: "hammer",
+  price: 9.99, 
+  description: "SMASH",
+  category: "tool"
+  },
+  {
+    name: "t-shirt",
+    price: 10.99,
+    description: "You wear this!",
+    category: "clothing"
+  },
+  {
+    name: "slacks",
+    price: 29.99,
+    description: "For formal events",
+    category: "clothing"
   }
+
 ]
 
 
